405000f6ed6e114d88ace4a6130f42a15cc68d0d
Aditi Kawale <aditikawale57@gmail.com>
2026-04-07 00:31:45 +0530

    JWT implemented successfully

--- src/main/java/com/interviewPlatform/filters/JwtFilter.java
+++ src/main/java/com/interviewPlatform/filters/JwtFilter.java
@@ -32,6 +32,8 @@ public class JwtFilter extends OncePerRequestFilter{
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
+
+                
        String authHeader=request.getHeader("Authorization");
        String token=null;
        String username=null;
